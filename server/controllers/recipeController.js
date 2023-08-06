c746f47b79d7f2299acab4367d367ac40f7f98c3
rajshukla444500 <rajj444500@gmail.com>
2023-08-06 16:26:07 +0530

    project work updated on 6Aug,2023

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,9 +1,66 @@
+require('../models/database');
+const Category = require('../models/Category');
+
+
 /**
  * GET /
  * Homepage
  */
 
 exports.HomePage = async(req, res)=>{
+
+
+        try {
+                const limitNumber = 5;
+                const categories = await Category.find({}).limit(limitNumber);
+                res.render('index',{title: 'HomePage',categories });
+
+
+        } catch (error) {
+                res.satus(500).send({message : error.message || "Error Occured"});
+        }
         
-        res.render('index',{title: 'HomePage'});
+      
 }
+
+
+
+
+// async function insertDymmyCatergoryData(
+
+// ){
+
+// try {
+//         await Category.insertMany(
+
+// [
+//         {
+//                 "name":"Pizza",
+//                 "image":"american-food.jpg"
+
+//         },
+//         {
+//                 "name":"paratha",
+//                 "image":"indian-food.jpg"
+
+//         },
+//         {
+//                 "name":"burger",
+//                 "image":"american-food.jpg"
+
+//         },
+//         {
+//                 "name":"paneer",
+//                 "image":"indian-food.jpg"
+
+//         },
+
+
+// ]);
+        
+// } catch (error) {
+//         console.log('err',+ error);
+        
+// }
+// }
+// insertDymmyCatergoryData();
